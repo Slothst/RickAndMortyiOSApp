f202fe0d14cec74b81463cc3b1a3e3699c858efd
Slothst <cnj991279@gmail.com>
2024-02-23 18:30:22 +0900

    create settings view with swiftUI

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by 최낙주 on 2/23/24.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+}
