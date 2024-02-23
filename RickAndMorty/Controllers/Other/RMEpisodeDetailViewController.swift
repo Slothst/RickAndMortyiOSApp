35ebad05167b46be622596600a9daaf413c1fff2
Slothst <cnj991279@gmail.com>
2024-02-23 15:55:05 +0900

    Episode detail cells

--- RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 /// VC to show details about single episode
-final class RMEpisodeDetailViewController: UIViewController {
+final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailViewViewModelDelegate {
     
     private let viewModel: RMEpisodeDetailViewViewModel
     
@@ -17,7 +17,7 @@ final class RMEpisodeDetailViewController: UIViewController {
     //MARK: - Init
     
     init(url: URL?) {
-        self.viewModel = .init(endpointUrl: url)
+        self.viewModel = RMEpisodeDetailViewViewModel(endpointUrl: url)
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -29,11 +29,13 @@ final class RMEpisodeDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         view.addSubview(detailView)
         addConstraints()
         title = "Episode"
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
+        viewModel.delegate = self
+        viewModel.fetchEpisodeData()
     }
     
     private func addConstraints() {
@@ -47,5 +49,11 @@ final class RMEpisodeDetailViewController: UIViewController {
     
     @objc private func didTapShare() {
         
+    }
+    
+    //MARK: - Delegate
+    
+    func didFetchEpisodeDetails() {
+        detailView.configure(with: viewModel)
     }
 }
---
+++ RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeInfoCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RMEpisodeInfoCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by 최낙주 on 2/22/24.
+//
+
+import Foundation
+
+struct RMEpisodeInfoCollectionViewCellViewModel {
+    public let title: String
+    public let value: String
+}
