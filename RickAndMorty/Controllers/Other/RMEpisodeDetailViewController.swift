361ba64a8f498356699a134e480fc9ace1a42cb9
Slothst <cnj991279@gmail.com>
2024-02-23 16:22:03 +0900

    Finish episode detail

--- RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 /// VC to show details about single episode
-final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailViewViewModelDelegate {
+final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailViewViewModelDelegate, RMEpisodeDetailViewDelegate {
     
     private let viewModel: RMEpisodeDetailViewViewModel
     
@@ -32,6 +32,7 @@ final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailView
         view.backgroundColor = .systemBackground
         view.addSubview(detailView)
         addConstraints()
+        detailView.delegate = self
         title = "Episode"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
         viewModel.delegate = self
@@ -51,7 +52,16 @@ final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailView
         
     }
     
-    //MARK: - Delegate
+    //MARK: - View Delegate
+    
+    func rmEpisodeDetailView(_ detailView: RMEpisodeDetailView, didSelect character: RMCharacter) {
+        let vc = RMCharacterDetailViewController(viewModel: .init(character: character))
+        vc.title = character.name
+        vc.navigationItem.largeTitleDisplayMode = .never 
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    //MARK: - ViewModel Delegate
     
     func didFetchEpisodeDetails() {
         detailView.configure(with: viewModel)
