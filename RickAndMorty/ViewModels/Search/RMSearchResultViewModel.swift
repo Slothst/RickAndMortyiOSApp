f27e44c7cade7668f8fec16e67bc966cc7f1ac8b
Slothst <cnj991279@gmail.com>
2024-03-02 22:10:10 +0900

    Character/Episode Search Pagination

--- RickAndMorty/ViewModels/Search/RMSearchResultViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMSearchResultViewModel.swift
@@ -47,7 +47,66 @@ final class RMSearchResultViewModel {
         
         RMService.shared.execute(
             request,
-            expecting: RMGetAllLocationsResponse.self) { [weak self] result in
+            expecting: RMGetAllLocationsResponse.self
+        ) { [weak self] result in
+            guard let strongSelf = self else {
+                return
+            }
+            switch result {
+            case .success(let responseModel):
+                let moreResults = responseModel.results
+                let info = responseModel.info
+                strongSelf.next = info.next // Capture new pagination url
+                
+                let additionalLocations = moreResults.compactMap {
+                    return RMLocationTableViewCellViewModel(location: $0)
+                }
+                var newResults: [RMLocationTableViewCellViewModel] = []
+                
+                switch strongSelf.results {
+                case .locations(let existingResults):
+                    newResults = existingResults + additionalLocations
+                    strongSelf.results = .locations(newResults)
+                    break
+                case .characters, .episodes:
+                    break
+                }
+                
+                DispatchQueue.main.async {
+                    strongSelf.isLoadingMoreResults = false
+                    strongSelf.didFinishPagination?()
+                    completion(newResults)
+                }
+            case .failure(let failure):
+                print(String(describing: failure))
+                strongSelf.isLoadingMoreResults = false
+            }
+        }
+    }
+    
+    public func fetchAdditionalResults(completion: @escaping ([any Hashable]) -> Void) {
+        guard !isLoadingMoreResults else {
+            return
+        }
+        
+        guard let nextUrlString = next,
+              let url = URL(string: nextUrlString) else {
+            return
+        }
+        
+        isLoadingMoreResults = true
+        
+        guard let request = RMRequest(url: url) else {
+            isLoadingMoreResults = false
+            return
+        }
+        
+        switch results {
+        case .characters(let existingResults):
+            RMService.shared.execute(
+                request,
+                expecting: RMGetAllCharactersResponse.self
+            ) { [weak self] result in
                 guard let strongSelf = self else {
                     return
                 }
@@ -57,20 +116,17 @@ final class RMSearchResultViewModel {
                     let info = responseModel.info
                     strongSelf.next = info.next // Capture new pagination url
                     
-                    let additionalLocations = moreResults.compactMap {
-                        return RMLocationTableViewCellViewModel(location: $0)
+                    let additionalResults = moreResults.compactMap {
+                        return RMCharacterCollectionViewCellViewModel(
+                            characterName: $0.name,
+                            characterStatus: $0.status,
+                            characterImageUrl: URL(string: $0.image)
+                        )
                     }
-                    var newResults: [RMLocationTableViewCellViewModel] = []
-                    
-                    switch strongSelf.results {
-                    case .locations(let existingResults):
-                        newResults = existingResults + additionalLocations
-                        strongSelf.results = .locations(newResults)
-                        break
-                    case .characters, .episodes:
-                        break
-                    }
-                    
+                    var newResults: [RMCharacterCollectionViewCellViewModel] = []
+                    newResults = existingResults + additionalResults
+                    strongSelf.results = .characters(newResults)
+                                    
                     DispatchQueue.main.async {
                         strongSelf.isLoadingMoreResults = false
                         strongSelf.didFinishPagination?()
@@ -81,6 +137,43 @@ final class RMSearchResultViewModel {
                     strongSelf.isLoadingMoreResults = false
                 }
             }
+        case .episodes(let existingResults):
+            RMService.shared.execute(
+                request,
+                expecting: RMGetAllEpisodesResponse.self
+            ) { [weak self] result in
+                guard let strongSelf = self else {
+                    return
+                }
+                switch result {
+                case .success(let responseModel):
+                    let moreResults = responseModel.results
+                    let info = responseModel.info
+                    strongSelf.next = info.next // Capture new pagination url
+                    
+                    let additionalResults = moreResults.compactMap {
+                        return RMCharacterEpisodeCollectionViewCellViewModel(episodeDataUrl: URL(string: $0.url))
+                    }
+                    var newResults: [RMCharacterEpisodeCollectionViewCellViewModel] = []
+                    newResults = existingResults + additionalResults
+                    strongSelf.results = .episodes(newResults)
+                                        
+                    DispatchQueue.main.async {
+                        strongSelf.isLoadingMoreResults = false
+                        strongSelf.didFinishPagination?()
+                        completion(newResults)
+                    }
+                case .failure(let failure):
+                    print(String(describing: failure))
+                    strongSelf.isLoadingMoreResults = false
+                }
+            }
+        case .locations:
+            // TableView case
+            break
+        }
+        
+        
     }
 }
 
